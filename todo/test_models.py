91f29939e6de95c3ebec2c1be688a1501ee81232
JakCrane <jackx.crane@hotmail.com>
2023-03-16 15:16:05 +0000

    finished coverage

--- todo/test_models.py
+++ todo/test_models.py
@@ -6,3 +6,7 @@ class TestModels(TestCase):
     def test_done_default_to_false(self):
         item = Item.objects.create(name='Test Todo Item')
         self.assertFalse(item.done)
+
+    def test_item_string_method_returns_name(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertEqual(str(item), 'Test Todo Item')
